830c841a4c6ba813a3168eb133936976603df72f
ipshita patra <patrarangadhar73@gmail.com>
2025-08-31 07:22:15 +0000

    make the background image more visible

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,7 +27,7 @@ export default function RootLayout({
               alt="background"
               fill
               style={{ objectFit: 'cover' }}
-              className="opacity-10"
+              className="opacity-25"
               data-ai-hint="abstract colorful"
             />
         </div>
