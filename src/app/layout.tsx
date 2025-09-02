cd371de265705bf5e914e568b823c58919ce227f
Heet Patel <heetpatel1896@gmail.com>
2025-09-02 22:29:39 +0000

    Resolve this

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,7 +21,7 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet" />
       </head>
-      <body className="font-body antialiased">
+      <body className="font-body antialiased" suppressHydrationWarning>
         <div className="fixed inset-0 z-[-1]">
           <Image
             src="https://drive.google.com/uc?export=view&id=1QH2yxa47s9VaCWJSVc1z8mdHxAQF3KOP"
