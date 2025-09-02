10253142f8bde41d6acd04da0c2d8cb271cbcb43
Prachi Singh <prachi0512.ps@gmail.com>
2025-09-02 11:17:07 +0000

    increase the opacity

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,7 +28,7 @@ export default function RootLayout({
               alt="background"
               fill
               style={{ objectFit: 'cover' }}
-              className="opacity-5"
+              className="opacity-10"
               data-ai-hint="blurry classroom"
             />
         </div>
