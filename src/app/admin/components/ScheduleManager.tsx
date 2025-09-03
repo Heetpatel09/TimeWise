b959f0c91d5d11eaf97776635d7c8056723a7201
Heet Patel <heetpatel1896@gmail.com>
2025-09-03 01:32:23 +0000

    Lecture slots can be scheduled between it.

--- src/app/admin/components/ScheduleManager.tsx
+++ src/app/admin/components/ScheduleManager.tsx
@@ -138,10 +138,11 @@ export default function ScheduleManager() {
 
   const days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'];
   const times = [
-    '09:00 AM - 10:00 AM', 
-    '10:00 AM - 11:00 AM', 
-    '11:00 AM - 12:00 PM', 
-    '12:00 PM - 01:00 PM', 
+    '07:30 AM - 08:30 AM',
+    '08:30 AM - 09:30 AM',
+    '10:00 AM - 11:00 AM',
+    '11:00 AM - 12:00 PM',
+    '01:00 PM - 02:00 PM',
     '02:00 PM - 03:00 PM'
   ];
 
