e5d7315ea715172d2dfb5ff19c362685117a4eb5
ipshita patra <patrarangadhar73@gmail.com>
2025-09-03 06:45:13 +0000

    https://sdmntprwestus.oaiusercontent.com/files/00000000-1c10-6230-9d4d-4

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,12 +24,12 @@ export default function RootLayout({
       <body className="font-body antialiased" suppressHydrationWarning>
         <div className="fixed inset-0 z-[-1]">
           <Image
-            src="https://storage.googleapis.com/studio-hosting-storage/v1/b/prj-B4Z_1s2V-pPz8a/files/9b3f36a8-a3f2-498c-a99f-7c7d425f1847"
+            src="https://sdmntprwestus.oaiusercontent.com/files/00000000-1c10-6230-9d4d-4aea9d2202ef/raw?se=2025-09-03T07%3A43%3A27Z&sp=r&sv=2024-08-04&sr=b&scid=89e684b5-8c1d-5c5a-868e-5f87a86ae48f&skoid=5939c452-ea83-4420-b5b4-21182254a5d3&sktid=a48cca56-e6da-484e-a814-9c849652bcb3&skt=2025-09-03T04%3A35%3A08Z&ske=2025-09-04T04%3A35%3A08Z&sks=b&skv=2024-08-04&sig=tz%2B2cUyEmZYPfjwZTJcJMfudEOUoLM5mmjXiOKq107Y%3D"
             alt="Abstract background"
             fill
             style={{ objectFit: 'cover' }}
             priority
-            data-ai-hint="abstract background"
+            data-ai-hint="abstract gradient"
           />
         </div>
         <Providers>
