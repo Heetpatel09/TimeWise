1305bad8a8ea52decd934e09e31aa3ec8b6a0a1e
ipshita patra <patrarangadhar73@gmail.com>
2025-09-04 08:36:52 +0000

    increase the opacity a little bit more pls

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,7 +27,7 @@ export default function RootLayout({
             src="https://sdmntprwestus.oaiusercontent.com/files/00000000-1c10-6230-9d4d-4aea9d2202ef/raw?se=2025-09-03T07%3A43%3A27Z&sp=r&sv=2024-08-04&sr=b&scid=89e684b5-8c1d-5c5a-868e-5f87a86ae48f&skoid=5939c452-ea83-4420-b5b4-21182254a5d3&sktid=a48cca56-e6da-484e-a814-9c849652bcb3&skt=2025-09-03T04%3A35%3A08Z&ske=2025-09-04T04%3A35%3A08Z&sks=b&skv=2024-08-04&sig=tz%2B2cUyEmZYPfjwZTJcJMfudEOUoLM5mmjXiOKq107Y%3D"
             alt="Abstract background"
             fill
-            style={{ objectFit: 'cover', opacity: 0.8 }}
+            style={{ objectFit: 'cover', opacity: 1 }}
             priority
             data-ai-hint="abstract gradient"
           />
