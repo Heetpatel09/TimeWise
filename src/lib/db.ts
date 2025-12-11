77e76be205e3cd3b87fac2455a572b5e39c224d8
ipshita patra <patrarangadhar73@gmail.com>
2025-12-11 17:20:47 +0000

    PLS MAKE A COULMN CALLED SGPA

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -24,7 +24,7 @@ const dbFilePath = './timewise.db';
 
 // A flag to indicate if the schema has been checked in the current run.
 let schemaChecked = false;
-const schemaVersion = 25; // Increment this to force re-initialization
+const schemaVersion = 26; // Increment this to force re-initialization
 const versionFilePath = path.join(process.cwd(), 'db-version.txt');
 
 
@@ -303,5 +303,7 @@ export { getDb as db };
 
 
       
+
+    
 
     
