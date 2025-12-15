4f30c6dc7c07bcc76a7bc7462f371ac1a8012803
ipshita patra <patrarangadhar73@gmail.com>
2025-12-15 16:32:49 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- next.config.ts
+++ next.config.ts
@@ -67,7 +67,7 @@ const nextConfig: NextConfig = {
     ],
   },
   experimental: {
-    instrumentationHook: true,
+    
   },
   webpack: (config) => {
     config.externals.push('pino-pretty', 'lokijs', 'encoding');
