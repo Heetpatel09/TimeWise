b06689385fd6006f11f41e83620759342e2e25f6
ipshita patra <patrarangadhar73@gmail.com>
2025-12-19 16:48:15 +0000

    still the same error

--- src/ai/flows/generate-timetable-flow.ts
+++ src/ai/flows/generate-timetable-flow.ts
@@ -61,7 +61,7 @@ const generateTimetable = ai.defineFlow(
 
     const llmResponse = await ai.generate({
       prompt: prompt,
-      model: googleAI.model('gemini-1.5-flash-latest'),
+      model: googleAI.model('gemini-pro'),
       output: {
         schema: GenerateTimetableOutputSchema,
       },
