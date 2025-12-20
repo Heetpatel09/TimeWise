2c93fdd9dca1c9a24c2a9934e2d00312c404e13b
ipshita patra <patrarangadhar73@gmail.com>
2025-12-20 20:07:09 +0000

    change v1 beta to v1

--- next.config.ts
+++ next.config.ts
@@ -61,12 +61,6 @@ const nextConfig: NextConfig = {
       }
     ],
   },
-  devIndicators: {
-    allowedDevOrigins: [
-        '*.cluster-a6zx3cwnb5hnuwbgyxmofxpkfe.cloudworkstations.dev',
-        '6000-firebase-studio-1756557245547.cluster-a6zx3cwnb5hnuwbgyxmofxpkfe.cloudworkstations.dev'
-    ],
-  },
   experimental: {
     
   }
