f1ea8af4a67b5d5978bbee96e3f0111ac148ba76
ipshita patra <patrarangadhar73@gmail.com>
2025-12-20 19:57:36 +0000

    try this api key AIzaSyDyEPz8P_Vqrk5ugXpG050qz-nOj0sdqRo

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -9,7 +9,7 @@ import {googleAI} from '@genkit-ai/google-genai';
 // the googleAI() plugin without any arguments will automatically use the
 // service account credentials of the runtime.
 export const ai = genkit({
-  plugins: [googleAI({ apiKey: "AQ.Ab8RN6JCy4HZBryDFGo3IYpLavE6lP_AFwFqNvFYQyHJP_astQ" })],
+  plugins: [googleAI({ apiKey: "AIzaSyDyEPz8P_Vqrk5ugXpG050qz-nOj0sdqRo" })],
   logLevel: 'debug',
   enableTracingAndMetrics: true,
 });
